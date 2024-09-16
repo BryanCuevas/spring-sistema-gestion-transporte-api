a6618e765322398bc23d4516cb0a1a38c250e2ac
Bryan Cuevas <bdcc.familia@gmail.com>
2024-09-16 18:46:07 -0500

    Commit inicial

---
+++ sistema-gestion-transporte-api/src/main/java/com/sise/sistema_gestion_transporte_api/SistemaGestionTransporteApiApplication.java
@@ -0,0 +1,13 @@
+package com.sise.sistema_gestion_transporte_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionTransporteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionTransporteApiApplication.class, args);
+	}
+
+}
