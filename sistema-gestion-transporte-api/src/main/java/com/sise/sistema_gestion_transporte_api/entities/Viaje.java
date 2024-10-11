9e1e8fdb156645cf551411d44f8eae64ac616758
Bryan Cuevas <bdcc.familia@gmail.com>
2024-10-11 18:09:49 -0500

    Relaciones con entidades Incidencia y ViajeCarga agregadas

--- sistema-gestion-transporte-api/src/main/java/com/sise/sistema_gestion_transporte_api/entities/Viaje.java
+++ sistema-gestion-transporte-api/src/main/java/com/sise/sistema_gestion_transporte_api/entities/Viaje.java
@@ -1,16 +1,19 @@
 package com.sise.sistema_gestion_transporte_api.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -57,6 +60,14 @@ public class Viaje {
     @Column(name = "fecha_creacion", insertable = false, updatable = false)
     @JsonIgnore
     private LocalDateTime fechaCreacion;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "viaje")
+    @JsonIgnore
+    private List<Incidencia> incidencias;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "viaje")
+    @JsonIgnore
+    private List<ViajeCarga> viajes;
 
     public void setVehiculo(Integer idVehiculo) {
         this.vehiculo = new Vehiculo();
